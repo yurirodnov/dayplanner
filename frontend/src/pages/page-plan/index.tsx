2f736adbec685308aad2e6b2830e9d0a685f3a84
yuri_rodnov <somewaystodo@yandex.ru>
2024-11-10 20:28:02 +0300

    add auth condition for planning page

--- frontend/src/pages/page-plan/index.tsx
+++ frontend/src/pages/page-plan/index.tsx
@@ -1,35 +1,52 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { FC } from "react";
 import { isUserAuthorised } from "../../app/store";
 import sleep from "../../assets/icons/sleep.svg"
 import "./pageplan.css";
 
 
 
-export const PagePlan = () => {
+export const PagePlan: FC = () => {
   const isAuthorised = isUserAuthorised.get();
 
-  console.log(isAuthorised)
+  if (!isAuthorised) {
+    return (
+      <div className="planning-block">
+        <div className="not-authorised-block">
+          <p>Войдите или зарегистрируйтесь, чтобы управлять задачами</p>
+        </div>
 
+        {/* <div className="no-task-block">
+          <div><h1>Пока ни одной задачи на день...</h1></div>
 
-  return (
-    <div className="planning-block">
-      <div className="not-authorised-block">
-        <p>Войдите или зарегистрируйтесь, чтобы управлять задачами</p>
+          <img className="sleep-svg" src={sleep} style={{ width: "90px" }} />
+        </div>
+
+        <div className="tasks-block"></div>
+
+        <button>Добавить задачу</button> */}
       </div>
+    )
+  } else {
+    return (
+      <div className="planning-block">
+        <div className="not-authorised-block">
+          <p>Войдите или зарегистрируйтесь, чтобы управлять задачами</p>
+        </div>
 
-      <div className="no-task-block">
-        <div><h1>Пока ни одной задачи на день...</h1></div>
+        <div className="no-task-block">
+          <div><h1>Пока ни одной задачи на день...</h1></div>
 
-        <img className="sleep-svg" src={sleep} style={{ width: "90px" }} />
+          <img className="sleep-svg" src={sleep} style={{ width: "90px" }} />
+        </div>
+
+        <div className="tasks-block"></div>
+
+        <button>Добавить задачу</button>
       </div>
+    )
 
-      <div className="tasks-block"></div>
-
-      <button>Добавить задачу</button>
-    </div>
+  }
 
 
-  )
 }
 
