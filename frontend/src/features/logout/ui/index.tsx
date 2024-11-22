ee0efb31c8d31035d6375f423e3235f8931127ce
yuri_rodnov <somewaystodo@yandex.ru>
2024-11-22 15:25:43 +0300

    add buttons for log-in/log-out state

---
+++ frontend/src/features/logout/ui/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+
+
+export const LogoutButton: FC = () => {
+  return (
+    <button>Выйти</button>
+  )
+}
--- frontend/src/widgets/user-enter/index.tsx
+++ frontend/src/widgets/user-enter/index.tsx
@@ -1,12 +1,23 @@
 import React, { FC } from "react";
 import { RegistrationButton } from "../../features/registration";
-import { AuthButton } from "../../features/auth";
+import { AuthButton } from "../../features/authorisation";
+import { LogoutButton } from "../../features/logout";
+import { isUserAuthorised } from "../../app/store";
+import "./user-enter.css"
 
 
-export const UserEnter = () => {
-  return (
-    <div className="user-enter-block">
-      <RegistrationButton />
-      <AuthButton />
+export const UserEnter: FC = () => {
+  const isAuthorised = isUserAuthorised.get();
+  if (isAuthorised) {
+    return (<div className="user-enter-block">
+      <LogoutButton />
     </div>)
+  } else {
+    return (
+      <div className="user-enter-block">
+        <RegistrationButton />
+        <AuthButton />
+      </div>)
+  }
+
 }
