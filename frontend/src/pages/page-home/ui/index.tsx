587ce4ba1060da0dba0b6520d001d00731b57558
yuri_rodnov <somewaystodo@yandex.ru>
2024-11-25 23:48:35 +0300

    fix welcome block layout

--- frontend/src/pages/page-home/ui/index.tsx
+++ frontend/src/pages/page-home/ui/index.tsx
@@ -1,19 +1,19 @@
 import React, { FC } from "react";
-
 import check from "../../../assets/icons/check.svg"
-import "./styles.module.css";
+import * as styles from "./styles.module.css";
 
 
 export const PageHome: FC = () => {
   return (
-    <div className="welcome-block">
+    <div className={styles["welcome-block"]}>
 
-      <h1>Добро пожаловать в планировщик дня!</h1>
+      <h1 className={styles["welcome-block-heading"]}>Добро пожаловать в планировщик дня!</h1>
 
       <div className="welcome-block description">
-        <div><p>Ставьте задачи</p><img src={check} style={{ width: "20px" }} /></div>
-        <div><p>Отслеживайте результат</p><img src={check} style={{ width: "20px" }} /></div>
-        <div><p>Достигайте целей</p><img src={check} style={{ width: "20px" }} /></div>
+        <div className={styles["welcome-block-description-point"]}><p>Ставьте задачи</p><img src={check} style={{ width: "20px" }} /></div>
+        <div className={styles["welcome-block-description-point"]}><p>Достигайте целей</p><img src={check} style={{ width: "20px" }} /></div>
+        <div className={styles["welcome-block-description-point"]}><p>Отслеживайте результат</p><img src={check} style={{ width: "20px" }} /></div>
+
       </div>
 
     </div>
