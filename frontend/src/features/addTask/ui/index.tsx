3dac041e6b2ce4a17ea912e7497a7c3956968b80
yuri_rodnov <somewaystodo@yandex.ru>
2024-12-07 09:03:12 +0300

    add module css for add task modal

--- frontend/src/features/addTask/ui/index.tsx
+++ frontend/src/features/addTask/ui/index.tsx
@@ -7,14 +7,14 @@ import * as styles from "./styles.module.css";
 export const AddTask = () => {
   console.log(`Current modal state is ${isAddTaskModalOpen.get()}`)
   const currentModalState = useHookstate(isAddTaskModalOpen)
-  const isModalOpen = currentModalState.get() ? "modal-grey-background" : "modal-grey-background-none";
+  const isModalOpen = currentModalState.get();
 
 
   return (
-    <div className={styles[isModalOpen]}>
-      <div className={styles["modal-window"]}>
+    <div className={isModalOpen ? styles.modalGreyBackground : styles.modalGreyBackgroundNone}>
+      <div className={styles.modalWindow}>
         <h3>Добавить задачу</h3>
-        <form className={styles["modal-window-form"]}>
+        <form className={styles.modalWindowForm}>
           <label htmlFor="task-name">Что планируете сделать:</label>
           <input type="text" id="task-name" />
           <label htmlFor="priority-select">Приоритет задачи:</label>
@@ -26,7 +26,7 @@ export const AddTask = () => {
           <label htmlFor="task-description">Описание задачи:</label>
           <textarea name="textarea" id="task-description"></textarea>
         </form>
-        <div className={styles["modal-window-buttons"]}>
+        <div className={styles.modalWindowButtons}>
           <button>Добавить</button>
           <button onClick={closeAddTaskModalHandler}>Отмена</button>
         </div>
